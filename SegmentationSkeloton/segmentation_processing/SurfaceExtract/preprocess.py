d6a2323ce6d7a5886eb7e10eee2b49cd17614353
sijinwu8 <sijinwu8@gmail.com>
2022-09-27 19:28:59 -0700

    fix preprocess.py to output all npy files

--- SegmentationSkeloton/segmentation_processing/SurfaceExtract/preprocess.py
+++ SegmentationSkeloton/segmentation_processing/SurfaceExtract/preprocess.py
@@ -10,8 +10,7 @@ import Model.constants_and_paths as mcp
 from Model.constants_and_paths import ROOT_STR
 
 alpha = 0.1
-
-index = 0
+n_samples = 21
 
 print('START')
 
@@ -19,30 +18,31 @@ data_dir = mcp.ROOT_STR
 
 # zarr_path = os.path.join(data_dir, "3Dexpandedprocessed.zarr")
 zarr_path = os.path.join(data_dir, "3Dexpanded.zarr")
+raw_data = zarr.open(zarr_path, mode='r')
 
-raw = zarr.open(zarr_path, mode='r')
-raw = raw['raw'][index]
-predict = np.load(data_dir+f'\\output_{index}.npy')
+for index in range(n_samples):
+	raw = raw_data['raw'][index]
+	predict = np.load(data_dir+f'\\output_{index}.npy')
 
-print("work well")
+	raw = np.copy(raw)
+	predict = np.squeeze(predict)
 
-raw = np.copy(raw)
-predict = np.squeeze(predict)
+	#print(np.shape(raw))
+	#print(np.shape(predict))
 
-#print(np.shape(raw))
-#print(np.shape(predict))
+	predict[predict <= alpha] = 0
+	predict[predict > alpha] = 1
 
-predict[predict <= alpha] = 0
-predict[predict > alpha] = 1
+	for i in range(np.shape(predict)[0]):
+		predict[i] = scipy.ndimage.morphology.binary_fill_holes(predict[i])
 
-for i in range(np.shape(predict)[0]):
-	predict[i] = scipy.ndimage.morphology.binary_fill_holes(predict[i])
+	raw[predict == 0] = 0
 
-raw[predict == 0] = 0
+	img = nib.Nifti1Image(raw, affine = np.eye(4))
+	nib.save(img, 'out.nii')
 
-img = nib.Nifti1Image(raw, affine = np.eye(4))
-nib.save(img, 'out.nii')
+	np.save(data_dir+'\\surface_'+str(index)+'.npy', raw)
 
-np.save(data_dir+'\\surface.npy', raw)
+	print(f"{index} is done")
 
 print('DONE')
